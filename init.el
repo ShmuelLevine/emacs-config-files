3c63db486ea3209ee65e59d77a16650c70772664
Tu Do <tu.h.do@dektech.com.au>
2014-08-28 10:56:45 +0700

    Revert swapping

--- init.el
+++ init.el
@@ -61,8 +61,8 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (delete 'company-semantic company-backends)
-(define-key c-mode-map  [(tab)] 'company-complete)
-(define-key c++-mode-map  [(tab)] 'company-complete)
+(define-key c-mode-map  [(control tab)] 'company-complete)
+(define-key c++-mode-map  [(control tab)] 'company-complete)
 
 (delete 'company-semantic company-backends)
 (define-key c-mode-map  [(tab)] 'company-complete)
